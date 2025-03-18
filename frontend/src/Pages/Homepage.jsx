50db81041812b04e6affbe031b2234c7595d5055
Charaka Jayamith <165627140+Charakajayamith2002@users.noreply.github.com>
2025-03-18 10:49:47 +0530

    update

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import LandingPage from '../Components/LandingPage'
-import Details from '../Components/Details'
+
 
 const Homepage = () => {
   return (
     <div>
       <LandingPage />
-      <Details />
+      {/* <Details /> */}
     </div>
   )
 }
