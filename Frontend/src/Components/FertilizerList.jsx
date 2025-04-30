387a89e65949f64ca0ddcc973ed2025d9beb7ca7
Mohamed Fasnas <mohamedfasnas999@gmail.com>
2025-04-30 08:32:33 +0530

    Add comments to FertilizerList.jsx

--- Frontend/src/Components/FertilizerList.jsx
+++ Frontend/src/Components/FertilizerList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from 'axios';// import axios
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const FertilizerList = () => {
